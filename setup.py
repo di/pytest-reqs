28111dd0448523ab7494e3e9d2b89f4f340e0169
Dustin Ingram <di@users.noreply.github.com>
2017-12-21 10:18:51 -0500

    Drop py33

--- setup.py
+++ setup.py
@@ -20,8 +20,8 @@ if __name__ == "__main__":
             'Framework :: Pytest',
             'Programming Language :: Python',
             'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
         ],
     )
