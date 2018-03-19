9fe361b5b276065f45f59ca059a8aa167eaea061
Dustin Ingram <di@users.noreply.github.com>
2018-03-19 10:26:44 -0500

    Pip version warning

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from setuptools import setup
 
 __version__ = '0.0.7'
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     setup(
         name='pytest-reqs',
         description='pytest plugin to check pinned requirements',
-        long_description=open("README.rst").read(),
-        license="MIT license",
+        long_description=open('README.rst').read(),
+        license='MIT license',
         version=__version__,
         author='Dustin Ingram',
         author_email='github@dustingram.com',
         url='https://github.com/di/pytest-reqs',
         py_modules=['pytest_reqs'],
         entry_points={'pytest11': ['reqs = pytest_reqs']},
-        install_requires=['pytest>=2.4.2'],
+        install_requires=['pytest>=2.4.2', 'packaging>=17.1'],
         tests_require=['pytest>=2.4.2', 'pretend'],
         classifiers=[
             'Framework :: Pytest',
