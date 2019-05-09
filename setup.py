8699caf52ad5d07a39f6ed97a86a9b1fc4915530
Dustin Ingram <di@users.noreply.github.com>
2019-05-09 12:56:06 -0400

    Drop support for Python 3.4

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ if __name__ == '__main__':
             'Framework :: Pytest',
             'Programming Language :: Python',
             'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
