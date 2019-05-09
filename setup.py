836d78ed2c37aae3a0261ce9e8cea8d7f6a9a5b7
Dustin Ingram <di@users.noreply.github.com>
2019-05-09 16:35:33 -0400

    Fix setup.py so tests pass

--- setup.py
+++ setup.py
@@ -2,26 +2,25 @@ from setuptools import setup
 
 __version__ = '0.2.1'
 
-if __name__ == '__main__':
-    setup(
-        name='pytest-reqs',
-        description='pytest plugin to check pinned requirements',
-        long_description=open('README.rst').read(),
-        license='MIT license',
-        version=__version__,
-        author='Dustin Ingram',
-        author_email='github@dustingram.com',
-        url='https://github.com/di/pytest-reqs',
-        py_modules=['pytest_reqs'],
-        entry_points={'pytest11': ['reqs = pytest_reqs']},
-        install_requires=['pytest>=2.4.2', 'packaging>=17.1', 'pip_api>=0.0.2'],
-        tests_require=['pytest>=2.4.2', 'pretend'],
-        classifiers=[
-            'Framework :: Pytest',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
-        ],
-    )
+setup(
+    name='pytest-reqs',
+    description='pytest plugin to check pinned requirements',
+    long_description=open('README.rst').read(),
+    license='MIT license',
+    version=__version__,
+    author='Dustin Ingram',
+    author_email='github@dustingram.com',
+    url='https://github.com/di/pytest-reqs',
+    py_modules=['pytest_reqs'],
+    entry_points={'pytest11': ['reqs = pytest_reqs']},
+    install_requires=['pytest>=2.4.2', 'packaging>=17.1', 'pip_api>=0.0.2'],
+    tests_require=['pytest>=2.4.2', 'pretend'],
+    classifiers=[
+        'Framework :: Pytest',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
